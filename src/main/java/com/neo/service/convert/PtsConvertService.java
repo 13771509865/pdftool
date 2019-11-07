ea1f568c465b6ecfba0b0613a8c5aa70a9c1dcce
xujun <xujun2764@163.com>
2019-11-07 14:07:25 +0800

    转换记录修改destFilePath

--- src/main/java/com/neo/service/convert/PtsConvertService.java
+++ src/main/java/com/neo/service/convert/PtsConvertService.java
@@ -87,7 +87,7 @@ public class PtsConvertService {
 			Map<String, Object> fcsMap= JsonUtils.parseJSON2Map(httpResult.getData().getBody());
 			
 			fcsFileInfoBO = JsonUtils.json2obj(fcsMap.get(SysConstant.FCS_DATA), FcsFileInfoBO.class);
-			SysLogUtils.info("ConvertType："+convertBO.getConvertType()+"==源文件相对路径"+convertBO.getSrcRelativePath()+"==fcs转码结果："+ fcsFileInfoBO.getCode());
+			SysLogUtils.info("ConvertType："+convertBO.getConvertType()+"==源文件相对路径:"+convertBO.getSrcRelativePath()+"==fcs转码结果："+ fcsFileInfoBO.getCode());
 			if(fcsFileInfoBO.getCode() == 0) {//转换成功
 				
 				updateFcsFileInfo(convertBO,fcsFileInfoBO, userId, ipAddress);//这里只记录转换成功的pts_convert
