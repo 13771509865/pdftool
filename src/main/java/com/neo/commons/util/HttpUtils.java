9556ba5016e7a9c30da7495d56a4d95c302ff6e4
xujun <xujun2764@163.com>
2019-12-19 09:11:36 +0800

    更新拦截器返回值格式

--- src/main/java/com/neo/commons/util/HttpUtils.java
+++ src/main/java/com/neo/commons/util/HttpUtils.java
@@ -320,7 +320,7 @@ public class HttpUtils {
 		response.setContentType("text/html;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
-		out.write(JsonResultUtils.fail(message));
+		out.write(message);
 		out.flush();
 		out.close();
 	}
