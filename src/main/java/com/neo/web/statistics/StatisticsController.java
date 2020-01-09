58889fe65de060aeaba81b7856ae409db12d1b25
xujun <xujun2764@163.com>
2020-01-09 17:38:16 +0800

    ip debug

--- src/main/java/com/neo/web/statistics/StatisticsController.java
+++ src/main/java/com/neo/web/statistics/StatisticsController.java
@@ -72,6 +72,7 @@ public class StatisticsController {
 	@GetMapping(value = "/convertTimes")
 	@ResponseBody
 	public Map<String,Object> getConvertTimes(HttpServletRequest request){
+		System.out.println(HttpUtils.getIpAddr(request));
 		IResult<String>  result = statisticsService.getConvertTimes(HttpUtils.getIpAddr(request),HttpUtils.getSessionUserID(request));
 		if(result.isSuccess()) {
 			return JsonResultUtils.successMapResult(result.getData());
