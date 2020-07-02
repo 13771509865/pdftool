06937861be0af4e1c7be6b37138d9626d8906ec1
xujun <xujun$276409819>
2020-07-02 09:22:57 +0800

    PDF统计转换失败文件详细信息

---
+++ src/main/java/com/neo/dao/PtsFailRecordPOMapper.java
@@ -0,0 +1,12 @@
+package com.neo.dao;
+
+import com.neo.model.po.PtsFailRecordPO;
+
+/**
+ * @Author: xujun
+ * @Date: 2020/7/1 2:02 下午
+ */
+public interface PtsFailRecordPOMapper {
+
+    int insertPtsFailRecord(PtsFailRecordPO ptsFailRecordPO);
+}
